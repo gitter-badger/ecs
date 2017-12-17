1a676954cecb4d896b6447487866a06819bc5755
Leopotam <leopotam@gmail.com>
2017-12-17 18:23:53 +0300

    * EcsWorld - docs. * Test - IEcsUpdateSystem example.

--- Assets/Ecs/EcsWorld.cs
+++ Assets/Ecs/EcsWorld.cs
@@ -3,22 +3,47 @@ using System.Collections.Generic;
 
 namespace LeopotamGroup.Ecs {
     public sealed class EcsWorld {
+        /// <summary>
+        /// All registered systems.
+        /// </summary>
         readonly List<EcsSystem> _allSystems = new List<EcsSystem> (64);
 
+        /// <summary>
+        /// Dictionary for fast search component <-> bitmask for component type.
+        /// </summary>
         readonly Dictionary<Type, ComponentMask> _componentMasks = new Dictionary<Type, ComponentMask> (64);
 
+        /// <summary>
+        /// List of all entities (their components).
+        /// </summary>
         readonly List<IEcsComponent[]> _entities = new List<IEcsComponent[]> (1024);
 
+        /// <summary>
+        /// List of removed entities - they can be reused later.
+        /// </summary>
         readonly List<int> _reservedEntities = new List<int> (256);
 
+        /// <summary>
+        /// Is Initialize method was called?
+        /// </summary>
         bool _inited;
 
+        /// <summary>
+        /// Adds new system to processing.
+        /// </summary>
+        /// <param name="system">System instance.</param>
         public EcsWorld AddSystem (EcsSystem system) {
+            if (_inited) {
+                throw new Exception ("Already initialized, cant add new system.");
+            }
             system.SetWorld (this);
             _allSystems.Add (system);
             return this;
         }
 
+        /// <summary>
+        /// Closes registration for new external data, initialize all registered systems.
+        /// </summary>
         public void Initialize () {
             _inited = true;
             for (int i = 0, iMax = _allSystems.Count; i < iMax; i++) {
@@ -26,6 +51,9 @@ namespace LeopotamGroup.Ecs {
             }
         }
 
+        /// <summary>
+        /// Destroys all registered external data, full cleanup for internal data.
+        /// </summary>
         public void Destroy () {
             for (var i = _allSystems.Count - 1; i >= 0; i--) {
                 _allSystems[i].Destroy ();
@@ -36,6 +64,9 @@ namespace LeopotamGroup.Ecs {
             _reservedEntities.Clear ();
         }
 
+        /// <summary>
+        /// Processing for IEcsUpdateSystem systems.
+        /// </summary>
         public void Update () {
             for (int i = 0, iMax = _allSystems.Count; i < iMax; i++) {
                 var updateSystem = _allSystems[i] as IEcsUpdateSystem;
@@ -45,6 +76,9 @@ namespace LeopotamGroup.Ecs {
             }
         }
 
+        /// <summary>
+        /// Processing for IEcsFixedUpdateSystem systems.
+        /// </summary>
         public void FixedUpdate () {
             for (int i = 0, iMax = _allSystems.Count; i < iMax; i++) {
                 var updateSystem = _allSystems[i] as IEcsFixedUpdateSystem;
@@ -54,6 +88,9 @@ namespace LeopotamGroup.Ecs {
             }
         }
 
+        /// <summary>
+        /// Creates new entity.
+        /// </summary>
         public int CreateEntity () {
             int entity;
             if (_reservedEntities.Count > 0) {
@@ -67,6 +104,10 @@ namespace LeopotamGroup.Ecs {
             return entity;
         }
 
+        /// <summary>
+        /// Removes exists entity or throws exception on invalid one.
+        /// </summary>
+        /// <param name="entity">Entity.</param>
         public void RemoveEntity (int entity) {
             if (entity < 0 || entity >= _entities.Count) {
                 throw new Exception ("Invalid entity");
@@ -77,15 +118,35 @@ namespace LeopotamGroup.Ecs {
             _reservedEntities.Add (entity);
         }
 
-        public T AddComponent<T> () where T : IEcsComponent {
+        /// <summary>
+        /// Adds component to entity.
+        /// </summary>
+        /// <param name="entity">Entity.</param>
+        public T AddComponent<T> (int entity) where T : IEcsComponent {
+            if (entity < 0 || entity >= _entities.Count) {
+                throw new Exception ("Invalid entity");
+            }
             throw new NotImplementedException ();
         }
 
-        public void RemoveComponent<T> () where T : IEcsComponent {
+        /// <summary>
+        /// Removes component from entity.
+        /// </summary>
+        /// <param name="entity">Entity.</param>
+        public void RemoveComponent<T> (int entity) where T : IEcsComponent {
+            if (entity < 0 || entity >= _entities.Count) {
+                throw new Exception ("Invalid entity");
+            }
             throw new NotImplementedException ();
         }
 
+        /// <summary>
+        /// For internal use only, dont call it directly.
+        /// </summary>
         public ComponentMask GetComponentsMask (Type[] types) {
+            if (_inited) {
+                throw new Exception ("Initialize method already called.");
+            }
             var mask = new ComponentMask ();
             if (types != null) {
                 for (var i = 0; i < types.Length; i++) {
@@ -95,8 +156,11 @@ namespace LeopotamGroup.Ecs {
             return mask;
         }
 
+        /// <summary>
+        /// For internal use only, dont call it directly.
+        /// </summary>
         public string GetDebugStats () {
-            return string.Format ("components: {0}, entities: {1}", _componentMasks.Count, _entities.Count);
+            return string.Format ("Components: {0}\nEntities: {1}", _componentMasks.Count, _entities.Count);
         }
 
         ComponentMask GetComponentMask (Type type) {
@@ -105,13 +169,8 @@ namespace LeopotamGroup.Ecs {
             }
             ComponentMask retVal;
             if (!_componentMasks.TryGetValue (type, out retVal)) {
-                if (!_inited) {
-                    retVal = ComponentMask.Create (_componentMasks.Count);
-                    _componentMasks[type] = retVal;
-                } else {
-                    // all systems initialized, no need to add new component types.
-                    retVal = new ComponentMask ();
-                }
+                retVal = ComponentMask.Create (_componentMasks.Count);
+                _componentMasks[type] = retVal;
             }
             return retVal;
         }
--- Assets/Test/TestSystems.cs
+++ Assets/Test/TestSystems.cs
@@ -12,13 +12,17 @@ namespace LeopotamGroup.Ecs.Tests {
         }
     }
 
-    public sealed class TestSystem2 : EcsSystem {
+    public sealed class TestSystem2 : EcsSystem, IEcsUpdateSystem {
         protected override Type[] GetRequiredComponents () {
             return new Type[] { typeof (WeaponComponent) };
         }
 
         public override void Initialize () {
             Debug.LogFormat ("{0}.requrements mask: {1}", GetType ().Name, ComponentsMask);
+        }
+
+        void IEcsUpdateSystem.Update () {
+            Debug.Log ("Update " + Time.time);
         }
     }
 }
