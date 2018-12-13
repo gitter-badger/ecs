c74e0f01e33e72700b78d17c1a89c70e989fc97c
Leopotam <leopotam@gmail.com>
2018-12-13 10:58:55 +0300

    * EcsWorld: Fix one-frame components created through EnsureComponent.

--- src/EcsWorld.cs
+++ src/EcsWorld.cs
@@ -311,7 +311,14 @@ namespace Leopotam.Ecs {
             for (var i = 0; i < entityData.ComponentsCount; i++) {
                 if (entityData.Components[i].Pool == pool) {
                     isNew = false;
-                    return (T) entityData.Components[i].Pool.GetExistItemById (entityData.Components[i].ItemId);
+                    return pool.Items[entityData.Components[i].ItemId];
+                }
+            }
+
+            // create separate filter for one-frame components.
+            if (pool.IsOneFrame) {
+                if (!_oneFrameFilters.ContainsKey (pool.GetComponentTypeIndex ())) {
+                    _oneFrameFilters[pool.GetComponentTypeIndex ()] = GetFilter (typeof (EcsFilter<T>));
                 }
             }
 
