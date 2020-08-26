a5b7d7306ec035663d3d8af59fa0e792efee41ce
Leopotam <leopotam@gmail.com>
2020-08-26 21:00:37 +0300

    * EcsComponent: Usings cleanup, unity-outside compilation fixed.

--- src/EcsComponent.cs
+++ src/EcsComponent.cs
@@ -5,10 +5,8 @@
 // ----------------------------------------------------------------------------
 
 using System;
-using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Threading;
-using JetBrains.Annotations;
 
 // ReSharper disable ClassNeverInstantiated.Global
 
